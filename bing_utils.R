502d4545d45e999ce009fb0b24125a17734033a3
Daniel Ferreira <bnmvco@gmail.com>
2020-05-21 19:19:31 -0300

    bing_utils: fix routines
    Actually make the (previously untested) bing_utils routines do stuff.

--- bing_utils.R
+++ bing_utils.R
@@ -5,9 +5,10 @@
 # See the LICENSE file for details.
 
 generate_bing_csv = function(name, filtered) {
-  filtered %>% %>% distinct(f_id)
+  filtered %>% distinct(f_id) %>% write.csv(paste0(name, '.csv'), row.names=F)
 }
 
 generate_devonthink_csv = function(name, filtered, bing_df) {
-  left_join(bing_df, filtered %>% select(f_id, SG_UF, NM_UE, DS_CARGOS), by=c('tse_id'='f_id'))
+  left_join(bing_df, filtered %>% select(f_id, SG_UF, NM_UE, DS_CARGOS), by=c('tse_id'='f_id')) %>%
+    write.csv(paste0(name, '.csv'), row.names=F)
 }
