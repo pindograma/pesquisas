4ce0766245f05df2140c05cedf888f7c7f8e7ffd
Daniel Ferreira <bnmvco@gmail.com>
2020-05-21 19:25:16 -0300

    bing-search.py: do proper exception handling

--- bing-search.py
+++ bing-search.py
@@ -25,22 +25,27 @@ with open('gapi-key-oscar.txt', 'r') as f:
 
             for tse_id in tse_ids:
                 print(tse_id)
-                web_data = client.web.search(
-                    query = '"{}"'.format(tse_id),
-                    count = 10,
-                    responseFilter = 'Webpages')
 
-                if hasattr(web_data.web_pages, 'value'):
-                    for page in web_data.web_pages.value:
-                        oup_dicts.append({
-                            'tse_id': tse_id,
-                            'url': page.url,
-                            'name': page.name,
-                            'snippet': page.snippet
-                        })
+                try:
+                    time.sleep(0.4)
+                    web_data = client.web.search(
+                        query = '"{}"'.format(tse_id),
+                        count = 10,
+                        responseFilter = 'Webpages')
+
+                    if hasattr(web_data.web_pages, 'value'):
+                        for page in web_data.web_pages.value:
+                            oup_dicts.append({
+                                'tse_id': tse_id,
+                                'url': page.url,
+                                'name': page.name,
+                                'snippet': page.snippet
+                            })
+                
+                except Exception as e:
+                    print(e)
+                    break
                         
-                time.sleep(0.4)
-
             with open('bing_output.csv', 'w', newline='\n', encoding='utf-8') as oup:
                 dict_writer = csv.DictWriter(oup, oup_dicts[0].keys(), lineterminator='\n')
                 dict_writer.writeheader()
